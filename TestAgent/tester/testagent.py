7e75a7027f16406c5d9beca2b3c6364e5ee206f1
LouluPE <lourdesnet@hotmail.com>
2016-06-30 22:26:52 -0400

    Update testagent.py

--- TestAgent/tester/testagent.py
+++ TestAgent/tester/testagent.py
@@ -1,3 +1,17 @@
+#This code has been developed for the 1st PNNL Connected Building Challenge
+#by the team "Ventilation Scheduler"
+#All our development is based on opensource contributions
+#It is intended to be distributed for the building automation communities for research and educational purposes
+#Copyrights and license is to be determined
+#This work is being funded by the Department of Energy Program - BIRD Building Integration Research and Developed Innovators Program
+#and NSF-I-Corps - Rochester Institute of Technology.
+#None of the team members, entities that had collaborated or funded to this project has legal liability 
+#or responsibility for any outcome
+#The views and opinions of any team member or collaborator do not reflect those of any agencyentity.
+#_Project Team: Lourdes Gutierrez and Priyank Kapadia
+#_Contact info: lmg4630@rit.edu, plk8075@rit.edu
+#_Created: 2016-06-19
+
 from __future__ import absolute_import
 from datetime import datetime,timedelta
 import logging
